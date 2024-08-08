1c95fbd5d3a079b8676b6d44db14000e28a99c76
Naomy25sandi <naomysandi25@gmail.com>
2024-08-07 21:14:05 -0600

    read me terminado

--- src/maquina.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Maquina:
-    def __init__(self):
-        self.intentos = []
-        self.colores_disponibles = ["red", "green", "yellow", "blue"]
-
-    def hacer_intento(self):
-       
